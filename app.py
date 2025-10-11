35639c024601ed91f267b375bbd2f842df5eb8b1
Samuel Vazquez <tuferbulots1234@gmail.com>
2025-10-10 21:13:16 -0600

    Creación de gráfica. Refactorización. Adición de Boostrap

--- app.py
+++ app.py
@@ -10,21 +10,18 @@ connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};D
 
 
 @app.route('/')
-def calcular_y_mostrar_regresion():
+def dashboard_regresion_ingresos():
     try:
         cnxn = pyodbc.connect(connection_string)
         sql_query = """
-            SELECT
-                Anio,
-                (ISNULL(IIS, 0) + ISNULL(ISC, 0) + ISNULL(IETE, 0) + ISNULL(IGTI, 0) + ISNULL(ITIC, 0) + ISNULL(LASC, 0)) AS TotalIngresos
-            FROM
-                Ingresos
-            ORDER BY
-                Anio;
-        """
+                    SELECT Anio,(ISNULL(IIS, 0) + ISNULL(ISC, 0) + ISNULL(IETE, 0) + ISNULL(IGTI, 0) + ISNULL(ITIC, 0) + ISNULL(LASC, 0)) AS TotalIngresos
+                    FROM Ingresos
+                    ORDER BY Anio;
+                    """
         df_ingresos = pd.read_sql(sql_query, cnxn)
         cnxn.close()
 
+        df_ingresos = df_ingresos[df_ingresos['TotalIngresos'] > 0]
         X = df_ingresos['Anio'].values.reshape(-1, 1)
         Y = df_ingresos['TotalIngresos'].values
 
@@ -34,7 +31,20 @@ def calcular_y_mostrar_regresion():
         beta_1 = modelo.coef_[0]
         beta_0 = modelo.intercept_
 
-        return render_template('index.html', ecuacion=f"Y = {beta_0:.2f} + {beta_1:.2f} * X", beta0=f"{beta_0:.2f}", beta1=f"{beta_1:.2f}")
+        linea_prediccion = modelo.predict(X)
+
+        anios = df_ingresos['Anio'].tolist()
+        ingresos_reales = df_ingresos['TotalIngresos'].tolist()
+        ingresos_predichos = linea_prediccion.tolist()
+
+        return render_template(
+            'index.html',
+            beta0=f"{beta_0:.2f}",
+            beta1=f"{beta_1:.2f}",
+            anios_json=anios,
+            ingresos_reales_json=ingresos_reales,
+            ingresos_predichos_json=ingresos_predichos
+        )
 
     except Exception as e:
         return f"<h1>Ocurrió un error en la aplicación</h1><p><strong>Detalle:</strong> {e}</p>"
